25b6d58ac3765770ffaa87243fba207ae5563801
atxrenegade <harleighabel@gmail.com>
2018-08-20 11:19:43 -0500

    edit and debug user_controller routes

--- app/controller/users_controller.rb
+++ app/controller/users_controller.rb
@@ -1,21 +1,25 @@
-get 'users/sign_up' do
-	erb :view
-end
+class UsersController < ActiveRecord::Base
 
-post 'users/sign_up' do
-	#validate and store params
-	erb index
-end
+	get 'users/sign_up' do
+		erb :view
+	end
 
-get 'users/login_in' do
-	erb :login_in
-end
+	get 'users/login_in' do
+		erb :login_in
+	end
 
-post 'users/sign_up' do
-	#validate and store params
-	erb :index
-end
+	post 'users/sign_up' do
+		#validate and store params
+		erb index
+	end
 
-get '/users/error' do
-	erb :error
+	get '/users/error' do
+		erb :error
+	end
+
+	post 'users/sign_up' do
+		#validate and store params
+		erb :index
+	end
+
 end
