d284bff5554151f9cc4c91698b93098643c2f63c
atxrenegade <harleighabel@gmail.com>
2018-09-17 19:59:10 -0500

    debug UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
 
-	get 'users/sign_up' do
+	get '/sign_up' do
 		erb :'/users/sign_up'
 	end
 
-	get 'users/login_in' do
+	get '/login_in' do
 		erb :'users/login_in'
 	end
 
-	post 'users/sign_up' do
+	post '/sign_up' do
 		#validate and store params
 		erb :'./index'
 	end
 
-	get '/users/error' do
-		erb :'users/error'
+	get '/error' do
+		erb :'/users/error'
 	end
 
-	post 'users/sign_up' do
+	post '/users/sign_up' do
 		#validate and store params
 		erb :'./index'
 	end
