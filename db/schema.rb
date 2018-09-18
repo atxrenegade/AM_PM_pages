f6c7dd9b4e1a80e6581c67a9d26e21a05eabcdd3
atxrenegade <harleighabel@gmail.com>
2018-09-17 19:55:06 -0500

    rename users password column to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_171639) do
+ActiveRecord::Schema.define(version: 2018_09_18_005143) do
 
   create_table "am_entries", force: :cascade do |t|
     t.datetime "time_date"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_171639) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
