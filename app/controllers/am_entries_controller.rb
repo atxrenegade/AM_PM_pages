556327cc87f6946e2db24b680018e751c4b1d9d0
atxrenegade <harleighabel@gmail.com>
2018-09-19 10:32:54 -0500

    create am_pages_view and debug corresponding controller action

--- app/controllers/am_entries_controller.rb
+++ app/controllers/am_entries_controller.rb
@@ -1,17 +1,26 @@
 require_relative 'application_controller.rb'
 class AMEntriesController < ApplicationController
+
 	get '/am_entries/new' do
 		erb :'am_entries/new'
 	end
 
+	get '/am_entries/show/:id' do
+		#binding.pry
+		@am_entry = AM_Entry.find_by_id(params[:id])
+		erb :'/am_entries/show'
+	end
+
 	post '/am_entries/new' do
-		binding.pry
+		#binding.pry
 		@time_date = "Month, Day Year  HH:MM:SS"
-		goals_array = params["goals"].values
-		gratitude_array = params["gratitude"].values
+		goals_array = params["goals"].collect{|k, v| v.strip}
+		#binding.pry
+		gratitude_array = params["gratitude"].collect{|k, v| v.strip}
+		binding.pry
 		@am_entry = AM_Entry.new(time_date: @time_date, goals: goals_array, awesome: params["awesome"], affirmation: params["affirmation"], gratitude: gratitude_array, words: params["words"])
 		@am_entry.save
 
-		redirect '/am_entries/#{@am_entry.id}/show'
+		erb :'/am_entries/show'
 	end
 end
