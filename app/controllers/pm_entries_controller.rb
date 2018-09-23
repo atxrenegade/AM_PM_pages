2db1745ab9068b51891cceab8cf8e9b5ae28cbbe
atxrenegade <harleighabel@gmail.com>
2018-09-23 16:55:51 -0500

    add current_ user.id validations to new,edit,delete,and show controller actions in pm_entries controller

--- app/controllers/pm_entries_controller.rb
+++ app/controllers/pm_entries_controller.rb
@@ -19,24 +19,16 @@ class PMEntriesController < ApplicationController
 		end
 	end
 
-	post '/pm_entries/new' do
-		if logged_in?
-			@pm_entry = PMEntry.new(excellent1: params["excellent1"], excellent2: params["excellent2"], lessons1: params["lessons1"], lessons2: params["lessons2"], gratitude1: params["gratitude1"], gratitude2: params["gratitude2"], gratitude3: params["gratitude3"], gratitude4: params["gratitude4"], gratitude5: params["gratitude5"])
-			@pm_entry.thoughts = params["thoughts"].strip
-			@pm_entry.user_id = session[:id]
-			@pm_entry.save
-			redirect "/pm_entries/show/#{@pm_entry.id}"
-		else
-			redirect '/login'
-		end
-	end
-
 	get '/pm_entries/show/:id' do
 		if logged_in?
 			@pm_entry = PMEntry.find_by_id(params[:id])
-			#replace with helper method format_date
-			@time_date = @pm_entry.created_at.localtime.to_formatted_s(:long_ordinal)
-			erb :'/pm_entries/show'
+			if @pm_entry && @pm_entry.user_id == current_user.id
+				#replace with helper method format_date
+				@time_date = @pm_entry.created_at.localtime.to_formatted_s(:long_ordinal)
+				erb :'/pm_entries/show'
+			else
+				redirect '/main_menu'
+			end
 		else
 			redirect '/login'
 		end
@@ -45,7 +37,27 @@ class PMEntriesController < ApplicationController
 	get '/pm_entries/:id/edit' do
 		if logged_in?
 			@pm_entry = PMEntry.find_by_id(params[:id])
-			erb :'/pm_entries/edit'
+			if @pm_entry && @pm_entry.user_id == current_user.id
+				erb :'/pm_entries/edit'
+			else
+				redirect '/main_menu'
+			end
+		else
+			redirect '/login'
+		end
+	end
+
+	post '/pm_entries/new' do
+		if logged_in?
+			@pm_entry = PMEntry.new(excellent1: params["excellent1"], excellent2: params["excellent2"], lessons1: params["lessons1"], lessons2: params["lessons2"], gratitude1: params["gratitude1"], gratitude2: params["gratitude2"], gratitude3: params["gratitude3"], gratitude4: params["gratitude4"], gratitude5: params["gratitude5"])
+			@pm_entry.thoughts = params["thoughts"].strip
+			@pm_entry.user_id = session[:id]
+			if @pm_entry && @pm_entry.user_id == current_user.id
+				@pm_entry.save
+				redirect "/pm_entries/show/#{@pm_entry.id}"
+			else
+				redirect '/main_menu'
+			end
 		else
 			redirect '/login'
 		end
@@ -54,9 +66,13 @@ class PMEntriesController < ApplicationController
 	patch '/pm_entries/:id' do
 		if logged_in?
 			@pm_entry = PMEntry.find_by_id(params[:id])
-			@pm_entry = PMEntry.update(params["excellent1"], params["excellent2"],  params["lessons1"], params["lessons2"], params["thoughts"].strip, params["gratitude1"], params["gratitude2"], params["gratitude3"], params["gratitude4"],  params["gratitude5"])
-			@pm_entry.save
-			redirect "/pm_entries/show/#{@pm_entry.id}"
+				if @pm_entry && @pm_entry.user_id == current_user.id
+				@pm_entry = PMEntry.update(params["excellent1"], params["excellent2"],  params["lessons1"], params["lessons2"], params["thoughts"].strip, params["gratitude1"], params["gratitude2"], params["gratitude3"], params["gratitude4"],  params["gratitude5"])
+				@pm_entry.save
+				redirect "/pm_entries/show/#{@pm_entry.id}"
+			else
+				redirect '/main_menu'
+			end		
 		else
 			redirect '/login'
 		end
@@ -65,10 +81,14 @@ class PMEntriesController < ApplicationController
 	delete '/pm_entries/:id/delete' do
 		if logged_in?
 			@pm_entry = PMEntry.find_by_id(params[:id])
-			@pm_entry.destroy
-			redirect '/index'
+			if @pm_entry && @pm_entry.user_id == current_user.id
+				@pm_entry.destroy
+				redirect '/index'
+			else
+				redirect '/main_menu'
+			end
 		else
 			redirect '/login'
-		end		
+		end
 	end
 end
