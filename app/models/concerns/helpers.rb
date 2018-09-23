9b8aaf78bb0b85dadeac3ae5cc0932996c1587d6
atxrenegade <harleighabel@gmail.com>
2018-09-22 21:19:06 -0500

    build out convert_time helper method

--- app/models/concerns/helpers.rb
+++ app/models/concerns/helpers.rb
@@ -8,7 +8,14 @@ class Helpers < ActiveRecord::Base
 	   User.find(session[:id])
    	end
 
-	def convert_time
-
+	def convert_time(entry)
+		if entry.is_a? AMEntry
+			time = AMEntry.created_at.localtime
+			time.to_formatted_s(:long_ordinal)
+		else
+			time = PMEntry.created_at.localtime
+			time.to_formatted_s(:long_ordinal)
+		end
+		#formatting target("%A %B %e %Y, %l:%m:%M %p")
 	end
 end
