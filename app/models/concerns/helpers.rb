d348477584fe7613a08bf29b347627cab4fc4b18
atxrenegade <harleighabel@gmail.com>
2018-09-26 12:58:32 -0500

    add heading to Welcome page, remove redundant methods from helper model

--- app/models/concerns/helpers.rb
+++ app/models/concerns/helpers.rb
@@ -1,13 +1,5 @@
 class Helpers < ActiveRecord::Base
 
-   	def logged_in?
-	   !!session[:id]
-   	end
-
-   	def current_user
-	   User.find(session[:id])
-   	end
-
 	def convert_time(entry)
 		time = entry.created_at.localtime
 		time.to_formatted_s(:long_ordinal)
